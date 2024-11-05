c1c9ef60a885d0ccb6a2fc7e178f42cd48ca001f
MartaSMarcos <MSanchezM@uxcale.com>
2024-11-05 19:02:58 +0100

    feat(index.js): meetups context added

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { FavoritesContextProvider } from './context/FavoritesContext';
+import { MeetupsContextProvider } from './context/MeetupsContext';
 
 ReactDOM.render(
     <FavoritesContextProvider>
-        <App />
+        <MeetupsContextProvider>
+            <App />
+        </MeetupsContextProvider>
     </FavoritesContextProvider>,
     document.getElementById('root')
 );
