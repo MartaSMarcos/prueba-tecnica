47545a701fed3e43f29852e879855034f5c2e37d
MartaSMarcos <MSanchezM@uxcale.com>
2024-11-05 19:08:17 +0100

    feat(meetups): add new meetups functionality added

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,9 +1,36 @@
+import { useRef } from 'react';
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
-export default function NewMeetupForm() {
-  function submitHandler(event) {
+export default function NewMeetupForm({ onAddMeetup }) {
+  const title = useRef();
+  const image = useRef();
+  const address = useRef();
+  const description = useRef();
+
+  async function submitHandler(event) {
     event.preventDefault();
+
+    const inputTitle = title.current.value;
+    const inputImage = image.current.value;
+    const inputAddress = address.current.value;
+    const inputDescription = description.current.value;
+
+    const meetupData = {
+      id: `m${Date.now()}`,
+      title: inputTitle,
+      image: inputImage,
+      address: inputAddress,
+      description: inputDescription,
+    };
+
+    console.log('NEW MEETUP FORM --> Meetup data:', meetupData);
+    
+    try {
+      await onAddMeetup(meetupData);
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
 
   return (
@@ -11,19 +38,24 @@ export default function NewMeetupForm() {
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={title} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={image} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={address} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea 
+            id="description" 
+            required 
+            rows="5" 
+            ref={description}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,27 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import MeetupsContext from '../context/MeetupsContext';
 
 export default function NewMeetupsPage() {
+  const navigate = useNavigate();
+  const meetContext = useContext(MeetupsContext);
+
+  async function addMeetupHandler(meetupData) {
+    try {
+      await meetContext.addMeetup(meetupData);
+
+      console.log('NewMeetupsPage --> Meetup added');
+      navigate('/');
+    } catch (error) {
+      console.error('NewMeetupsPage --> Error adding meetup:', error);
+    }
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
